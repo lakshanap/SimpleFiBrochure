1638b869ede0cb2b5356f8701a81eaa3c182d790
Andre Oschadlin <andreoschadlin@gmail.com>
2012-10-03 14:49:42 -0700

    Included feedback changes to main page and added share buttons

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
+	$("#transfer-table").hide();
+
 	$("#quiz-answer1").click(function(event){
-		$("#quiz-answer1").text("Sorry, would you believe this loan actually ends up costing an equivalent of 16%?");
+		$("#lesson").html("");
+		$("#quiz-answer").html("<div class='answer-text'>Sorry, would you believe the 'interest free' loan actually ends up costing an equivalent of 16% interest (Annualised Percentage Rate 'APR')?\
+			                       <br><br>You only pay $23 in interest with the 9% APR option as opposed to $40.</div>");
+		$("#btn-store").html("<div id='quiz-btn' style='center' class='btn btn-large'>How is that possible?</div>");
+		$("#quiz-btn").bind('click', function(event){
+			$("#quiz-table").html("<div class='answer-text'><img src='assets/repayment_chart.png'< /></div>");
+		});
 	});
 
 	$("#quiz-answer2").click(function(event){
-		$("#quiz-answer2").text("You're right! This is a far better option than the 4% upfront fee loan that ends up costing an equivalent of 16%");
+		$("#lesson").html("");
+		$("#quiz-answer").html("<div class='answer-text'>You're right! This is a far better option than the 4% upfront fee loan that ends up costing an equivalent of 16% interest (Annualised Percentage Rate 'APR')\
+														<br><br>You only pay $23 in interest with the 9% APR option as opposed to $40.</div>");
+		$("#btn-store").html("<div id='quiz-btn' style='center' class='btn btn-large'>How is that possible?</div>");
+		$("#quiz-btn").bind('click', function(event){
+			$("#quiz-table").html("<div class='answer-text'><img src='assets/repayment_chart.png'< /></div>");
+		});
+	});
+
+	$("#btn-transfer-table").click(function(event){
+		$("#transfer-table").show();
+		$("#btn-transfer-table").hide();
 	});
 });
